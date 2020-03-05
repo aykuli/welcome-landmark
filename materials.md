ed51e98305ad12ab1e6d3e9888b0f08d5556d7ed
aykuli <aykuli@ya.ru>
2020-03-05 23:14:11 +0700

    feat: add first build

--- materials.md
+++
@@ -1,7 +0,0 @@
-Materials used to build this app:
-
-1. [How to fetch data with React Hooks?](https://www.robinwieruch.de/react-hooks-fetch-data)
-2. [mapbox-gl from uber](https://uber.github.io/react-map-gl/docs/advanced/viewport-transition)
-3. [d3-ease](https://bl.ocks.org/d3noob/1ea51d03775b9650e8dfd03474e202fe)
-4. [Markers](https://docs.mapbox.com/mapbox-gl-js/api/#marker)
-5. [Examples](https://github.com/uber/react-map-gl/blob/5.2-release/examples/locate-user/src/app.js)
---
+++ precache-manifest.1da17a7a9f82d14e146b290a39f98d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2f36b7dd93ccb34425a961d68f66e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d142a2d03f93db20d73d",
+    "url": "/static/css/2.3b80cb7c.chunk.css"
+  },
+  {
+    "revision": "d142a2d03f93db20d73d",
+    "url": "/static/js/2.de95f7af.chunk.js"
+  },
+  {
+    "revision": "61205513b340a875f7a6bee554d1cdfb",
+    "url": "/static/js/2.de95f7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "694238e2d8682b9e83a6",
+    "url": "/static/js/main.8fae91bf.chunk.js"
+  },
+  {
+    "revision": "d58b6456056b03247402",
+    "url": "/static/js/runtime-main.40bdc7a0.js"
+  }
+]);
